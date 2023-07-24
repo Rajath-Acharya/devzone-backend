7ec4ec8ef16fd2c29b49d18d7048660e03984902
Rajath-Acharya <rajath365@gmail.com>
2023-07-24 18:32:44 +0530

    Running graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('Server page')
+import { buildSchema } from 'graphql'
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+
+const PORT = 4000
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+const app = express()
+
+const root = {
+  hello: () => {
+    return 'Hello world!'
+  },
+}
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+)
+
+app.listen(PORT, () => {
+  console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`)
+})
